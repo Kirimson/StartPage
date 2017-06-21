029d4216eddeff4549684604959bcc032327c155
Kirimson <kierangates@outlook.com>
2017-06-21 22:00:12 +0100

    Made settings better

--- js/preferencewindow.js
+++ js/preferencewindow.js
@@ -1,5 +1,21 @@
 var clicked = false;
 
+var late = "#333";
+var darkLate = "#444";
+var morning = "#21332B";
+var darkMorning = "#253930"
+var afternoon = "#16222a";
+var darkAfternoon = "#19262F";
+var evening = "#2b2241";
+var darkEvening = "#39324B";
+
+var textAreas = document.getElementsByTagName('textarea');
+
+Array.prototype.forEach.call(textAreas, function(elem) {
+	console.log("woo2");
+    elem.placeholder = elem.placeholder.replace(/\\n/g, '\n');
+});
+
 try{
 	var LinkData = JSON.parse($.cookie("personal-links"));
 } catch(err) {
@@ -9,6 +25,25 @@ try{
 function toggleSettings(){
 	if(!clicked){
 		$('#settingsmain').css('display', 'block');
+
+		var currTime = timeCheck();
+
+		if(currTime == 1){
+			$('#settingsmain').css('background-color', late);
+			$('#settinglinks').css('background-color', darkLate);
+		}
+		if(currTime == 2){
+			$('#settingsmain').css('background-color', morning);
+			$('#settinglinks').css('background-color', darkMorning);
+		}
+		if(currTime == 3){
+			$('#settingsmain').css('background-color', afternoon);
+			$('#settinglinks').css('background-color', darkAfternoon);
+		}
+		if(currTime == 4){
+			$('#settingsmain').css('background-color', evening);
+			$('#settinglinks').css('background-color', darkEvening);
+		}
 	}
 	else
 	{
@@ -17,12 +52,19 @@ function toggleSettings(){
 	clicked=!clicked;
 }
 
+$( "#settinglinks" ).keydown(function( e ) {
+	    if (e.keyCode == 27) {
+	        toggleSettings();
+	    }
+	});
+
 if(typeof LinkData == 'undefined')
 {
 	toggleSettings();
 }
 else
 {
+	console.log("From the cookie");
 	console.log(LinkData);
 
 	jQuery.each(LinkData.titles, function(index, el) {
@@ -32,7 +74,13 @@ else
 		}
 	});
 
+
 	jQuery.each(LinkData.links, function(index1, el1) {
+
+		if(index1==0){$('#settinglinks').append('\\');}
+		$('#settinglinks').append(LinkData.titles[index1]);
+		$('#settinglinks').append('\n');
+
 		jQuery.each(el1, function(index2, el2) {
 			jQuery.each(el2, function(index3, el3) {
 				$('#settinglinks').append(el3);
@@ -41,9 +89,6 @@ else
 		});
 
 		$('#settinglinks').append('\\');
-		if(index1 != LinkData.links.length-1){
-			$('#settinglinks').append('\n');
-		}
 	});
 
 }
@@ -60,8 +105,7 @@ $('#settingsclose').click(function(){
 $('#settingssubmit').click(function(){
 	var LinkData = { }
 
-	var titles = $('#settingtitles').val().split('\n');
-	var titlelength = titles.length;
+	var titles = [];
 
 	var links = [];
 
@@ -71,15 +115,25 @@ $('#settingssubmit').click(function(){
 	var linksforarray = [];
 	for(var i = 0; i < linklength; i = i+2)
 	{
-		if(templinks[i] != "\\") 
+		if(templinks[i].charAt(0) != "\\") 
 		{
 			var linkdetails = [templinks[i], templinks[i+1]];
 			linksforarray.push(linkdetails);
 		}
 		else
 		{
-			links.push(linksforarray);
-			linksforarray = [];
+			if(i != linklength-1)
+			{
+				var sectiontitle = templinks[i].substr(1);
+				titles.push(sectiontitle);
+			}
+
+			if(i !=0 )
+			{
+				links.push(linksforarray);
+				linksforarray = [];
+			}
+
 			i = i-1;
 		}
 
@@ -93,6 +147,7 @@ $('#settingssubmit').click(function(){
 	LinkData.titles = titles;
 	LinkData.links = links;
 
+	console.log("From the textarea");
 	console.log(LinkData);
 
 	$.cookie("personal-links", JSON.stringify(LinkData), { expires: 365 });
